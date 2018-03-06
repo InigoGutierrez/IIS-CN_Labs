3ba0c3567fdb96f85349b958661972ebbb7b4379
InigoGutierrez <inigogf.95@gmail.com>
2018-03-06 18:16:34 +0100

    Solved Exercise 8 from Lab02

--- 02_ZerosOfNonlinearFunctions/02Exercise8.py
+++ 02_ZerosOfNonlinearFunctions/02Exercise8.py
@@ -9,22 +9,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize as opt
 
-f = lambda x : 75**(1/3)    # define the function
-df = lambda x : np.cos(x) * np.sinh(x) - np.cosh(x) * np.sin(x)
+f = lambda x : x**3 - 75    # define the function
 
 # prepare the graphic representation of the function
-x = np.linspace(-1,20,num=200)               # define the mesh
+x = np.linspace(0,5,num=200)               # define the mesh
 y = f(x)
 OX = 0*x                            # define X axis
 
 tol = 1.e-12
 maxiter = 100
 
+a = 3
+b = 5
 # find the zeroes with scipy
-x0 = opt.newton(f,17,tol=tol,maxiter=maxiter)
+x0 = opt.bisect(f,a,b,xtol=tol,maxiter=maxiter)
 
 # plot the zeroes and show the graph
-plt.title('Exercise 7 - scipy',fontsize=18)
+plt.title('Exercise 8 - scipy',fontsize=18)
 plt.plot(x,y,label='f function')
 plt.plot(x,OX,'k-')
 plt.plot([x0],[0], 'ro', label='zeroes')
@@ -32,20 +33,22 @@ plt.legend()
 plt.show()
 
 # find the zeroes with custom newton function
-def secant(f,x0,x1,tol = 1.e-12,maxiter = 200):
+def bisection(f,a,b,tol = 1.e-12, maxiter = 200):
     n = 0
-    sol = x0
-    while n < maxiter and abs(x1-x0) > tol:
+    sol = (a+b)/2.
+    while n < maxiter and abs(b-sol) > tol:
         n += 1
-        sol = x1 - f(x1)*((x1-x0)/(f(x1)-f(x0)))
-        x0 = x1
-        x1 = sol
-    return (sol, n)
+        if f(a) * f(sol) < 0:
+            b = sol
+        else:
+            a = sol
+        sol = (a+b)/2.
+    return (sol,n)
 
-x0, n = secant(f,17,20,tol,maxiter)
+x0, n = bisection(f,a,b,tol,maxiter)
 
 # plot the zeroes and show the graph
-plt.title('Exercise 7 - custom function',fontsize=18)
+plt.title('Exercise 8 - custom function',fontsize=18)
 plt.plot(x,y,label='f function')
 plt.plot(x,OX,'k-')
 plt.plot([x0],[0], 'ro', label='zeroes')
