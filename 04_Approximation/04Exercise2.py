546b73e5b5d656b90218837ecddaae0e61c93db2
InigoGutierrez <inigogf.95@gmail.com>
2018-03-20 19:38:04 +0100

    Solved Exercises 1 and 2 from Lab04

---
+++ 04_Approximation/04Exercise2.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 20 19:33:00 2018
+
+@author: UO238186
+"""
+
+from __future__ import division
+import numpy as np
+import matplotlib.pyplot as plt
+
+a = -1
+b = 1
+n = 10
+d = 4
+
+x = np.linspace(a,b,n)
+f = lambda x: np.cos(np.arctan(x)) - np.exp(x**2)*np.log(x+2)
+
+y = f(x)
+A = np.zeros((n,d+1))
+for i in range(n):
+   for j in range(d+1):
+       A[i,j] = x[i]**j
+At = A.T
+
+coefficientMatrix = np.dot(At,A)
+rightHandSideMatrix = np.dot(At,y)
+solution = np.linalg.solve(coefficientMatrix,rightHandSideMatrix)
+
+print("Coefficient matrix")
+print(coefficientMatrix)
+print()
+print("Right hand side matrix")
+print(rightHandSideMatrix)
+print()
+print("System solution")
+print(solution)
+
+xx = np.linspace(a,b)
+yy = np.polyval(solution[::-1],xx)
+plt.plot(xx,yy, 'b-', label='approximating polynomial')
+plt.plot(x,y, 'ro', label='nodes')
+plt.legend()
+plt.title('Exercise 2',fontsize=18)
+plt.show()
